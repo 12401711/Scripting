710e50f2350ebe5df79c4b8ce443bdc997474ba1
michielmondelaers <12401711@pxl.be>
2024-10-02 11:02:01 +0200

    oefening 8 deel 2

---
+++ Deel 2/Oef8.js
@@ -0,0 +1,15 @@
+
+
+
+let getal1 = parseFloat(prompt("Geef het eerste gehele getal in:"));
+let getal2 = parseFloat(prompt("Geef het tweede gehele getal in:"));
+
+let vermenigvuldigen = getal1 * getal2;
+let som = getal1 + getal2;
+let aftrekken = getal1 - getal2;
+let delen = getal1 / getal2;
+
+document.body.innerHTML += "<p>Resultaat vermenigvuldigen: <br>" + vermenigvuldigen +"</p>";
+document.body.innerHTML += "<p>Resultaat som: <br>" + som +"</p>";
+document.body.innerHTML += "<p>Resultaat aftrekken: <br>" + aftrekken +"</p>";
+document.body.innerHTML += "<p>Resultaten delen: <br>" + delen +"</p>";
