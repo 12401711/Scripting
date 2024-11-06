7f33d78a12cfd2859b32c5ddf1e12a7b42fdf9fc
michielmondelaers <12401711@pxl.be>
2024-11-06 11:07:13 +0100

    Oefening 1.3

---
+++ Deel 3/Arrays/Oef1.3.js
@@ -0,0 +1,8 @@
+const opleidingen = ["Toegepaste Informatica", "Elektronica-ICT",
+    "Digitale Vormgeving", "Programmeren", "Internet of Things", "Systeem- en Netwerkbeheer",
+    "Artificial Intelligence Business Architect", "Drones Business Architect",
+    "Smart Cities Business Architect"];
+
+const opleidingenString = opleidingen.join("<br>");
+
+document.body.innerHTML = opleidingenString;
