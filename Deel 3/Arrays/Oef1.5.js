837013cd57aad966fa7ee3e2131460f52cd86f0f
michielmondelaers <12401711@pxl.be>
2024-11-06 11:36:54 +0100

    Oefening 1.5

---
+++ Deel 3/Arrays/Oef1.5.js
@@ -0,0 +1,16 @@
+const hondenrassen = [
+    "Labrador Retriever",
+    "Duitse Herder",
+    "Golden Retriever",
+    "Beagle",
+    "Bulldog",
+    "Poedel",
+    "Chihuahua",
+    "Rottweiler",
+    "Boxer",
+    "Husky"
+];
+
+hondenrassen.unshift("Shih Tzu", "Cocker Spaniel", "Dobermann", "Dalmatian", "Akita");
+
+console.log(hondenrassen);
