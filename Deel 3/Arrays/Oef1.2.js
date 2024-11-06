af626f5406762e3b8be89336edc44efd7d96a0af
michielmondelaers <12401711@pxl.be>
2024-11-06 11:00:04 +0100

    Oefening 1.2

---
+++ Deel 3/Arrays/Oef1.2.js
@@ -0,0 +1,9 @@
+const opleidingen = ["Toegepaste Informatica", "Elektronica-ICT",
+    "Digitale Vormgeving", "Programmeren", "Internet of Things", "Systeem- en Netwerkbeheer",
+    "Artificial Intelligence Business Architect", "Drones Business Architect",
+    "Smart Cities Business Architect"];
+
+opleidingen.push("Bachelor Multimedia- en Communicatietechnologie");
+
+console.log(opleidingen);
+console.log(opleidingen.length);
