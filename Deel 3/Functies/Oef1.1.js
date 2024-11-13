5368ef6393b45ab37d55037aef3b03d8751cc5d2
michielmondelaers <12401711@pxl.be>
2024-11-13 11:51:20 +0100

    Hoofdstuk functies Oefening 1.1

---
+++ Deel 3/Functies/Oef1.1.js
@@ -0,0 +1,11 @@
+function vermenigvuldiging() {
+
+        let getal1 = 5;
+        let getal2 = 3;
+
+        let resultaat = getal1 * getal2;
+
+    document.getElementById("output").innerText = "Het resultaat is: " + resultaat;
+}
+
+vermenigvuldiging();
