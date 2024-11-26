732f988ca3a1e44a462daf818030ef62fefde064
michielmondelaers <12401711@pxl.be>
2024-11-26 18:12:39 +0100

    H3 Objecten - oefening 4

--- Deel 3/Functies/Oef1.4.js
+++ Deel 3/Functies/Oef1.4.js
@@ -1,12 +1,12 @@
 let zin = prompt("Geef een zin in:");
 
 function startMetHallo() {
-    let zin = prompt("Geef een zin in:");
 
     if (zin.startsWith("Hallo")){
         console.log(zin);
     } else {
-        let aangepasteZin = "Hallo " + zin.charAt(0).toLowerCase() + zin.slice(1);
-        console.log(aangepasteZin);
+        let nieuweZin = "Hallo " + zin.charAt(0).toLowerCase() + zin.slice(1);
+        console.log(nieuweZin);
     }
 }
+startMetHallo(zin);
